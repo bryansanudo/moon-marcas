19244fdb9abc04327f2b9d64e7f65c8d61a1520b
Bryan Sanudo Garcia <bryansanudo95@gmail.com>
2023-02-17 10:22:37 -0500

    first deploy

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import MobileBanner from "@/components/MobileBanner";
+function App() {
+  const [isMenuShown, setIsMenuShown] = useState(false);
+
+  return (
+    <>
+      <Navbar isMenuShown={isMenuShown} setIsMenuShown={setIsMenuShown} />
+      <HeroSection isMenuShown={isMenuShown} />
+      <MobileBanner />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MobileBanner.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import logoSara from "@/assets/logoSara.jpg";
+import logoMdwx from "@/assets/logoMdwx.jpg";
+import logoJmarioCouture from "@/assets/logoJmarioCouture.jpg";
+import logoMagicripped from "@/assets/logoMagicripped.jpg";
+import logoVoufit from "@/assets/logoVoufit.jpg";
+import logoEliana from "@/assets/logoEliana1.jpg";
+import logoVapeshop from "@/assets/logoVapeshop.jpg";
+import logoMoon from "@/assets/logoMoon.jpg";
+const MobileBanner = () => {
+  const links = [
+    {
+      id: 10,
+      link: "https://eliana-vapeshop.vercel.app/",
+      src: logoMoon,
+      brand:
+        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-thPrimary hover:shadow-thPrimary duration-300 cursor-pointer hover:shadow-xl shadow-md",
+    },
+    {
+      id: 6,
+      link: "https://eliana-vapeshop.vercel.app/",
+      src: logoVapeshop,
+      brand:
+        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#35EEED] hover:shadow-[#35EEED] duration-300 cursor-pointer hover:shadow-xl shadow-md",
+    },
+    {
+      id: 2,
+      link: "https://eliana-maxwax.vercel.app/",
+      src: logoMdwx,
+      brand:
+        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-[#29fd04] hover:shadow-[#29fd04]   duration-300 cursor-pointer hover:shadow-xl shadow-md",
+    },
+    {
+      id: 4,
+      link: "https://eliana-portafolio.vercel.app/",
+      src: logoEliana,
+      brand:
+        "rounded-full object-cover w-20 h-20 md:w-40 md:h-40 shadow-thPrimary hover:shadow-thPrimary duration-300 cursor-pointer hover:shadow-xl shadow-md",
+    },
+  ];
+  return (
+    <div className=" lg:hidden flex items-center justify-center py-8 md:gap-5 gap-2">
+      {links.map(({ id, link, src, brand }) => (
+        <a key={id} href={link}>
+          <div>
+            <img src={src} alt="" className={brand} />
+          </div>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default MobileBanner;
